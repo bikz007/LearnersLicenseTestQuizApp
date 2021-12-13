ffc3a08241e9dbce6ea44fa7f4db6e68292ac201
Modak,Bikram <bikrammodak75@gmail.com>
2021-12-13 14:30:39 +0530

    Remove qna db

--- result_analyzer.py
+++ result_analyzer.py
@@ -3,6 +3,8 @@ import json
 import os
 import pandas as pd
 import sys
+from matplotlib import pyplot as plt
+import seaborn as sns
 sys.dont_write_bytecode = True
 # change these as per total number of questions in questions and answers database
 TOTAL_NUM_OF_QUESTIONS = 140
@@ -17,8 +19,7 @@ for res_file in res_json_files:
     with open(res_file) as json_file:
         json_text = json.load(json_file)
         for item in json_text['results']:
-            item_dict = {"qNumber": [item['qNumber']], 'answerSelected': [
-                int(item['answerSelected'])], 'correctAnswer': [int(item['correctAnswer'])], 'isCorrect': [item['isCorrect']]}
+            item_dict = {"qNumber": [item['qNumber']], 'isCorrect': [item['isCorrect']]}
             # read data frame from json file
             df = pd.DataFrame.from_dict(item_dict)
             list_of_df.append(df)  # append the data frame to the list
@@ -32,10 +33,17 @@ def unique_ques(df):
 
 def questions_not_attempted(df):
     uniq_q_num_series = df.qNumber
+    # print(sorted(list(uniq_q_num_series)))
     res = series_of_actual_qnum[~series_of_actual_qnum.isin(uniq_q_num_series)]
+    print(sorted(list(res)))
     return res
 
 uniq_ques_df = unique_ques(final_df)
 
 pp("Unique number of questions attempted so far: {}".format(uniq_ques_df.qNumber.size))
 pp("Number of questions not attempted so far: {}".format(questions_not_attempted(uniq_ques_df).size))
+frequency = final_df['qNumber'].value_counts()
+qnum_freq_df = pd.concat([uniq_ques_df['qNumber'],frequency.reset_index(drop=True)],axis=1)
+qnum_freq_df.columns = ['qNumber', 'frequency']
+# pp(qnum_freq_df[qnum_freq_df['frequency'] < 2].count())
+pp(qnum_freq_df.sort_values('frequency',ascending=False).to_string())
